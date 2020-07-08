f97726dfb826b3ab4a89358b6d0d2260078b7826
Joe Jasinski <joe.jasinski@gmail.com>
2020-07-08 02:01:18 -0500

    update parser

--- k8s_user/__main__.py
+++ k8s_user/__main__.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from .user import K8sUser
 import kubernetes
@@ -10,8 +11,11 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('name', nargs='?')
 parser.add_argument('-d', "--directory", help='Key Dir')
+parser.add_argument('-c', "--context", help="context name", default="default")
 args = parser.parse_args()
 print(args)
 api_client = config.new_client_from_config()
 user = K8sUser(name=args.name, key_dir=args.directory)
 user.create(api_client)
+user.config(cluster_name="joe", context_name=args.context)
+user.save_config(os.path.join(args.directory, "kubeconfig.yaml"))
